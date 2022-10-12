216d27f8517e8b0e0736407d87317ff26dbbc71c
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-10-12 12:10:04 -0300

    feat: change addUser use case to accept new user format

--- src/use-cases/user/add-user.ts
+++ src/use-cases/user/add-user.ts
@@ -1,38 +1,41 @@
 import { Database } from '../../infra/database'
 import { User } from '../../models/user'
 
-type AddUserParams = Omit<User, 'id'> & {
-  subsidiary: { id: string }
-  address: { id: string }
-}
+type AddUserParams = Omit<User, 'id' | 'demissionDate'>
 
-export class AddUser {
-  async add (params: AddUserParams): Promise<User> {
-    const user = await Database.getInstance().insert('user', {
-      name: params.name,
-      ctps: params.ctps,
-      cpf: params.cpf,
-      admission_date: params.admissionDate,
-      demission_date: params.demissionDate ?? null,
-      contact_phone: params.contactPhone,
-      role_name: params.role,
-      subsidiary_id: params.subsidiary.id,
-      address_id: params.address.id
-    })
+export async function addUser (params: AddUserParams): Promise<User> {
+  const user = await Database.getInstance().insert('user', {
+    name: params.name,
+    ctps: params.ctps,
+    cpf: params.cpf,
+    admission_date: params.admissionDate,
+    contact_phone: params.contactPhone,
+    role_name: params.role,
+    sector: params.sector,
+    city: params.city,
+    neighborhood: params.neighborhood,
+    number: params.number,
+    postal_code: params.postalCode,
+    street: params.street
+  })
 
-    if (!user) {
-      throw new Error('Server error')
-    }
+  if (!user) {
+    throw new Error('Server error')
+  }
 
-    return {
-      admissionDate: user.admission_date,
-      contactPhone: user.contact_phone,
-      cpf: user.cpf,
-      ctps: user.ctps,
-      id: user.id,
-      name: user.name,
-      role: user.role_name,
-      demissionDate: user.demission_date
-    }
+  return {
+    admissionDate: user.admission_date,
+    contactPhone: user.contact_phone,
+    cpf: user.cpf,
+    ctps: user.ctps,
+    id: user.id,
+    name: user.name,
+    role: user.role_name,
+    city: user.city,
+    neighborhood: user.neighborhood,
+    number: user.number,
+    postalCode: user.postal_code,
+    sector: user.sector,
+    street: user.street
   }
 }
