4165a5efacba5ce55f09b79474cd7efd75c8f49c
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-10-12 12:43:33 -0300

    feat: add validation middlewares to express works properly

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import express, { Router } from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import { makeUserRoutes } from './routes/user-routes'
 
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
 
 const userRouter = Router()
 makeUserRoutes(userRouter)
 
 app.use('/user', userRouter)
 
-const PORT = 3000
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
 })
