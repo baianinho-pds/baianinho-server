2d73c627b003ed944270bc4a7b0642c05dbe95b7
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-10-12 12:41:44 -0300

    fix: change sector to correct database column name

--- src/use-cases/user/add-user.ts
+++ src/use-cases/user/add-user.ts
@@ -4,14 +4,14 @@ import { User } from '../../models/user'
 type AddUserParams = Omit<User, 'id' | 'demissionDate'>
 
 export async function addUser (params: AddUserParams): Promise<User> {
-  const user = await Database.getInstance().insert('user', {
+  const user = await Database.getInstance().insert('person', {
     name: params.name,
     ctps: params.ctps,
     cpf: params.cpf,
     admission_date: params.admissionDate,
     contact_phone: params.contactPhone,
     role_name: params.role,
-    sector: params.sector,
+    sector_name: params.sector,
     city: params.city,
     neighborhood: params.neighborhood,
     number: params.number,
@@ -24,18 +24,18 @@ export async function addUser (params: AddUserParams): Promise<User> {
   }
 
   return {
+    id: user.id,
     admissionDate: user.admission_date,
     contactPhone: user.contact_phone,
     cpf: user.cpf,
     ctps: user.ctps,
-    id: user.id,
     name: user.name,
     role: user.role_name,
     city: user.city,
     neighborhood: user.neighborhood,
     number: user.number,
     postalCode: user.postal_code,
-    sector: user.sector,
+    sector: user.sector_name,
     street: user.street
   }
 }
