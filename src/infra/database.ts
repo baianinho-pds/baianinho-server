41d8743c6ab88cc0e79c59966256d117112fa365
FilipeSouza <filipe.souza1602@gmail.com>
2022-10-16 00:24:45 -0300

    feat: add find many

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,5 +1,17 @@
 import knex, { Knex } from 'knex'
 
+type FindManyParams<T> = {
+  offset: number
+  limit: number
+  select: Array<keyof T>
+  count?: boolean 
+}
+
+export type Page<T> = {
+  data: T[]
+  total?: number
+}
+
 export class Database {
   private static instance: Database
   private readonly connection: Knex
@@ -31,5 +43,24 @@ export class Database {
       .into(table)
 
     return item
+  }
+
+  public async findMany<T>(table: string, data: FindManyParams<T>): Promise<Page<T>> {
+    const items = await this.connection.select(data.select)
+      .from(table)
+      .offset(data.offset)
+      .limit(data.limit)
+
+    if(data.count) {
+      const [{ count = 0 }] = await this.connection(table).count<{ count: string }>({ count: '*' })
+      return {
+        data: items,
+        total: count
+      }
+    } 
+
+    return {
+      data: items
+    }
   }
 }
