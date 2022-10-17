410abc89ae58861aa6360a5804d417f458f87ea1
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-10-16 22:05:30 -0300

    refactor: remove comments

--- src/infra/migrations/20221016134022_add_sector_table.ts
+++ src/infra/migrations/20221016134022_add_sector_table.ts
@@ -10,12 +10,3 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.dropTable('sector')
 }
-
-
-// table.increments('id')
-//     table.string('name', 255)
-//     table.string('ctps', 8)
-//     table.string('cpf', 11)
-//     table.date('admission_date')
-//     table.string('contact_phone', 11)
-//     table.string('role')
