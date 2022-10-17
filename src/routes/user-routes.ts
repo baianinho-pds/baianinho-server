a0053cbf9c4f222aa53d37a5f15df53567ab1698
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-10-16 22:21:42 -0300

    feat: add update user route

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,6 +3,7 @@ import { addUser } from '@/use-cases/user/add-user'
 import { deleteUser } from '@/use-cases/user/delete-user'
 import { findUser } from '@/use-cases/user/find-user'
 import { findUserPage } from '@/use-cases/user/find-user-page'
+import { updateUser } from '@/use-cases/user/update-user'
 import { Router } from 'express'
 
 export function makeUserRoutes (router: Router): void {
@@ -116,6 +117,82 @@ export function makeUserRoutes (router: Router): void {
     deleteUser(parseInt(userId)).then(() => {
       return res.status(204).send()
     }).catch((error) => {
+      console.error(error)
+      res.status(500).json({ error: 'ServerError' })
+    })
+  })
+
+  router.put('/:id', (req, res) => {
+    const validRoles: User.Role[] = ['admin', 'seller']
+    const validSectors: User.Sector[] = ['external', 'internal']
+    const {
+      name,
+      ctps,
+      cpf,
+      admission_date,
+      contact_phone,
+      role_name,
+      city,
+      neighborhood,
+      number,
+      postal_code,
+      sector_name,
+      street,
+      demission_date
+    } = req.body as User
+
+    const { id } = req.params
+
+
+    const isNameValid = name && typeof name === 'string'
+    const isCptsValid = ctps && typeof ctps === 'string'
+    const isCpfValid = cpf && typeof cpf === 'string'
+    const isAdmissionDateValid = admission_date && new Date(admission_date).getTime() > 0
+    const isContactPhoneValid = contact_phone && typeof contact_phone === 'string'
+    const isRoleValid = role_name && validRoles.includes(role_name)
+    const isCityValid = city && typeof city === 'string'
+    const isNeighborhoodValid = neighborhood && typeof neighborhood === 'string'
+    const isNumberValid = number && typeof number === 'string'
+    const isPostalCodeValid = postal_code && typeof postal_code === 'string'
+    const isSectorValid = sector_name && validSectors.includes(sector_name)
+    const isStreetValid = street && typeof street === 'string'
+    const isDemissionDateValid = !demission_date || (new Date(demission_date).getTime() > 0)
+    const isUserIdValid = !isNaN(parseInt(id)) 
+
+    if (
+      !isNameValid ||
+      !isCptsValid ||
+      !isCpfValid ||
+      !isAdmissionDateValid ||
+      !isContactPhoneValid ||
+      !isRoleValid ||
+      !isCityValid ||
+      !isNeighborhoodValid ||
+      !isNumberValid ||
+      !isPostalCodeValid ||
+      !isSectorValid ||
+      !isStreetValid ||
+      !isDemissionDateValid ||
+      !isUserIdValid
+    ) {
+      return res.status(400).json({ error: 'BadFormat' })
+    }
+
+    void updateUser({
+        id: parseInt(id),
+        admission_date,
+        contact_phone,
+        cpf,
+        ctps,
+        name,
+        role_name,
+        sector_name,
+        city,
+        neighborhood,
+        number,
+        postal_code,
+        street
+    }).then(user => res.status(200).json(user)).catch((error) => {
       console.error(error)
       res.status(500).json({ error: 'ServerError' })
     })
