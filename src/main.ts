5b0e21cb8efb4fcc7f845865b10274a7720724ad
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-11-23 19:56:20 -0300

    feature: add client router

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import { makePersonRoutes } from './routes/person-routes'
+import { makeClientRoutes } from './routes/client-routes'
 
 const app = express()
 
@@ -9,9 +10,13 @@ app.use(cors())
 app.use(bodyParser.json())
 
 const personRouter = Router()
+const clientRouter = Router()
+
 makePersonRoutes(personRouter)
+makeClientRoutes(clientRouter)
 
 app.use('/person', personRouter)
+app.use('/client', clientRouter)
 
 const PORT = 3001
 app.listen(PORT, () => {
