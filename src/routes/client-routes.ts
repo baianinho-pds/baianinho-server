e6b0a815ca1279f77c05bc39b7f5874aaadea582
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-11-24 22:29:04 -0300

    feat: add update client route

--- src/routes/client-routes.ts
+++ src/routes/client-routes.ts
@@ -3,6 +3,7 @@ import { addClient } from '@/use-cases/client/add-client'
 import { deleteClient } from '@/use-cases/client/delete-client'
 import { findClient } from '@/use-cases/client/find-client'
 import { findclientPage } from '@/use-cases/client/find-client-page'
+import { updateClient } from '@/use-cases/client/update-client'
 import { Router } from 'express'
 
 export function makeClientRoutes (router: Router): void {
@@ -86,6 +87,46 @@ export function makeClientRoutes (router: Router): void {
     findClient(parseInt(clientId)).then((clientPage) => {
       return res.status(200).json(clientPage)
     }).catch((error) => {
+      console.error(error)
+      res.status(500).json({ error: 'ServerError' })
+    })
+  })
+
+  router.put('/:id', (req, res) => {
+    const {
+      address,
+      name,
+      contact_phone,
+      cnpj,
+      cpf
+    } = req.body as Client
+
+    const { id } = req.params
+
+    const isAddressValid = address && typeof address === 'string'
+    const isContactPhoneValid = contact_phone && typeof contact_phone === 'string'
+    const isNameValid = name && typeof name === 'string'
+    const isCNPJValid = cnpj === undefined || cnpj === null || typeof cnpj === 'string'
+    const isCPFValid = cpf === undefined || cpf === null || typeof cpf === 'string'
+
+    if (
+      !isAddressValid ||
+      !isNameValid ||
+      !isCNPJValid ||
+      !isCPFValid ||
+      !isContactPhoneValid
+    ) {
+      return res.status(400).json({ error: 'BadFormat' })
+    }
+
+    void updateClient({
+      id: parseInt(id),
+      address,
+      name,
+      cnpj,
+      cpf,
+      contact_phone
+    }).then(client => res.status(200).json(client)).catch(error => {
       console.error(error)
       res.status(500).json({ error: 'ServerError' })
     })
