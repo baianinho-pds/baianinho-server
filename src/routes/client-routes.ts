4ace35faa018ff37059ec9a6fc83cbe60c175abb
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-11-24 21:44:28 -0300

    feat: add delete client route

--- src/routes/client-routes.ts
+++ src/routes/client-routes.ts
@@ -1,5 +1,6 @@
 import { Client } from '@/models/client'
 import { addClient } from '@/use-cases/client/add-client'
+import { deleteClient } from '@/use-cases/client/delete-client'
 import { Router } from 'express'
 
 export function makeClientRoutes (router: Router): void {
@@ -35,6 +36,23 @@ export function makeClientRoutes (router: Router): void {
       cpf,
       contact_phone
     }).then(client => res.status(200).json(client)).catch(error => {
+      console.error(error)
+      res.status(500).json({ error: 'ServerError' })
+    })
+  })
+
+  router.delete('/:id', (req, res) => {
+    const clientId = req.params.id
+
+    const isClientValid = !isNaN(parseInt(clientId))
+
+    if (!isClientValid) {
+      return res.status(400).json({ error: 'BadRequest' })
+    }
+
+    deleteClient(parseInt(clientId)).then(() => {
+      return res.status(204).send()
+    }).catch((error) => {
       console.error(error)
       res.status(500).json({ error: 'ServerError' })
     })
