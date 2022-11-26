c616799bf9f42941bb5f4ef478226b626c485646
Samuel <samuelalves681@gmail>
2022-11-25 22:31:27 -0300

    add routes for get all and one feedstock

--- src/routes/feedstock-route.ts
+++ src/routes/feedstock-route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { FeedStock } from '@/models/feedstock'
-import { addFeedStock } from '@/use-cases/feedstock'
+import { addFeedStock, findFeedStock, findFeedStockPage } from '@/use-cases/feedstock'
 
 export function makeFeedStockRoutes(router: Router): void {
   router.post('/', (req, res) => {
@@ -24,11 +24,39 @@ export function makeFeedStockRoutes(router: Router): void {
       return res.status(400).json({ error: 'BadFormat' })
     }
 
-    void addFeedStock({
+    addFeedStock({
       amount, name, provider, suppliesType, unit, validity
     }).then(feedstock => res.status(200).json(feedstock)).catch(error => {
       console.error(error)
       res.status(500).json({ error: 'ServerError' })
     })
+  })
+
+  router.get('/', (req, res) => {
+    const { itemsPerPage = '10', page = '1' } = req.query
+
+    findFeedStockPage({
+      itemsPerPage: parseInt(itemsPerPage as string),
+      page: parseInt(page as string)
+    }).then(feedstockPage => res.status(200).json(feedstockPage)).catch(error => {
+      console.error(error)
+      res.status(500).json({ error: 'ServerError' })
+    })
+  })
+
+  router.get('/:id', (req, res) => {
+    const feedstockId = req.params.id
+
+    const isFeedStockValid = !isNaN(parseInt(feedstockId))
+
+    if (!isFeedStockValid) {
+      return res.status(400).json({ error: 'BadFormat' })
+    }
+
+    findFeedStock(parseInt(feedstockId)).then(feedstock => res.status(200).json(feedstock))
+      .catch(error => {
+        console.error(error)
+        res.status(500).json({ error: 'ServerError' })
+      })
   })
 }
