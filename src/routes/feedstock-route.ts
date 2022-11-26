666fc8fc522580bb90387b56a0b451d274d8857f
Samuel <samuelalves681@gmail>
2022-11-25 22:41:58 -0300

    feat: add route update feedstock

--- src/routes/feedstock-route.ts
+++ src/routes/feedstock-route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { FeedStock } from '@/models/feedstock'
-import { addFeedStock, findFeedStock, findFeedStockPage } from '@/use-cases/feedstock'
+import { addFeedStock, findFeedStock, findFeedStockPage, updateClient } from '@/use-cases/feedstock'
 
 export function makeFeedStockRoutes(router: Router): void {
   router.post('/', (req, res) => {
@@ -58,5 +58,37 @@ export function makeFeedStockRoutes(router: Router): void {
         console.error(error)
         res.status(500).json({ error: 'ServerError' })
       })
+  })
+
+  router.put('/:id', (req, res) => {
+    const {
+      amount,
+      name,
+      provider,
+      suppliesType,
+      unit,
+      validity
+    } = req.body as FeedStock
+
+    const { id } = req.params
+
+    const isAmountValid = amount === undefined || amount === null || typeof amount === 'number'
+    const isNameValid = name && typeof name === 'string'
+    const isProviderValid = provider && typeof provider === 'string'
+    const isSuppliesTypeValid = suppliesType && typeof suppliesType === 'string'
+    const isUnitValid = unit && typeof unit === 'string'
+    const isValidytValid = validity === undefined || validity === null || validity && new Date(validity).getTime() > 0
+
+    if (!isAmountValid || !isNameValid || !isProviderValid || !isSuppliesTypeValid || !isUnitValid || !isValidytValid) {
+      return res.status(400).json({ error: 'BadFormat' })
+    }
+
+    updateClient({
+      id: parseInt(id),
+      amount, name, provider, suppliesType, unit, validity
+    }).then(feedstock => res.status(200).json(feedstock)).catch(error => {
+      console.error(error)
+      res.status(500).json({ error: 'ServerError' })
+    })
   })
 }
--- src/use-cases/feedstock/index.ts
+++ src/use-cases/feedstock/index.ts
@@ -1,3 +1,4 @@
 export * from './add-feedstock'
 export * from './find-feedstock'
 export * from './find-feedstock-page'
+export * from './update-feedstock'
