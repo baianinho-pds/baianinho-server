0269a43b1d7c99c19cbc2a0b9ee3439d8e3213c2
Iago Rocha Porto <iagorochaporto@gmail.com>
2022-11-30 19:45:37 -0300

    feat: add product_feedstock relation

--- src/models/feedstock.ts
+++ src/models/feedstock.ts
@@ -1,3 +1,5 @@
+import { Relation } from '@/models/relation'
+
 export interface FeedStock {
   id: number
   name: string
@@ -6,4 +8,19 @@ export interface FeedStock {
   unit: string
   amount?: number
   validity?: Date
+}
+
+export namespace FeedStock {
+  export const Relations: Relation[] = [{
+    type: 'many-to-many',
+    aux_table_name: 'product_feedstock',
+    joinColumn: {
+      columnName: 'feedstock_id',
+      referenceColumnName: 'id'
+    },
+    inverseJoinColumn: {
+      columnName: 'product_id',
+      referenceColumnName: 'id'
+    }
+  }]
 }
